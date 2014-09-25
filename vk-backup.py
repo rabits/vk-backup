2efb4f3b2a2b058bc79d687ea9bec958801d510c
Rabits <home@rabits.org>
2014-09-26 02:20:00 +0400

    Already can download & update dialogs of user

--- vk-backup.py
+++ vk-backup.py
@@ -2,20 +2,32 @@
 # -*- coding: UTF-8 -*-
 
 from lib import vk_auth
-import json
-import urllib2
+
 from urllib import urlencode
-import os
-import getpass
-import sys
-import codecs
-import time
-import datetime
+from time import time, sleep
+import getpass, codecs, urllib2, json
+import sys, os
+
+lastcall = 0
 
 def call_api(method, params, token):
-    params.append(("access_token", token))
-    url = "https://api.vk.com/method/%s?%s" % (method, urlencode(params))
-    return json.loads(urllib2.urlopen(url).read())["response"]
+    global lastcall
+    diff = time() - lastcall
+    if diff < 0.4:
+        sleep(0.4)
+    lastcall = time()
+
+    for retry in xrange(3):
+        params.append(("access_token", token))
+        url = "https://api.vk.com/method/%s?%s" % (method, urlencode(params))
+        data = json.loads(urllib2.urlopen(url).read())
+        if 'response' in data:
+            break
+        print 'WARNING: error while calling api method %s (retry %i)' % (method, retry)
+        print '  %s' % data
+        sleep(2.0*(retry+1))
+
+    return data["response"]
 
 def get_users(user_ids, token):
     return call_api("users.get", [("user_ids", ','.join(user_ids)), ("fields", "photo_max_orig")], token)
@@ -38,12 +50,14 @@ password = getpass.getpass()
 client_id = "2951857" # Vk application ID
 token, user_id = vk_auth.auth(email, password, client_id, "messages")
 
-current = 0
+now = long(time())
+
+counter = 0
 
 chats = {}
 users = {}
 while True:
-    dialogs = get_dialogs(current, token)
+    dialogs = get_dialogs(counter, token)
     count = long(dialogs.pop(0))
     for dialog in dialogs:
         if 'chat_id' in dialog:
@@ -54,35 +68,86 @@ while True:
             else:
                 print 'Found self user_id'
 
-    current += 200
-    if current > count:
-        current = 0
+    counter += 200
+    if counter >= count:
+        counter = 0
         break
 
 users_ids = users.keys()
 print 'Getting dialog users: %i' % len(users_ids)
 while True:
-    data = get_users(users_ids[current:current+1000], token)
+    data = get_users(users_ids[counter:counter+1000], token)
     for user in data:
-        print '  %i %s %s' % (user['uid'], user['first_name'], user['last_name'])
+        #print '  %i %s %s' % (user['uid'], user['first_name'], user['last_name'])
         users[str(user['uid'])] = user
 
-    current += 1000
-    if current > len(users_ids):
-        current = 0
+    counter += 1000
+    if counter >= len(users_ids):
+        counter = 0
         break
 
 chats_ids = chats.keys()
 print 'Getting chat info: %i' % len(chats_ids)
 while True:
-    data = get_chats(chats_ids[current:current+200], token)
+    data = get_chats(chats_ids[counter:counter+200], token)
     for chat in data:
-        print '  %i %s' % (chat['chat_id'], chat['title'])
+        #print '  %i %s' % (chat['chat_id'], chat['title'])
         chats[str(chat['chat_id'])] = chat
 
-    current += 200
-    if current > len(chats_ids):
-        current = 0
+    counter += 200
+    if counter >= len(chats_ids):
+        counter = 0
         break
+
+if not os.path.isdir("backup/dialogs"):
+    os.makedirs("backup/dialogs") 
+
+print 'Loading dialogs...'
+for key, user in users.items():
+    print '  %i %s %s:' % (user['uid'], user['first_name'], user['last_name'])
+
+    filename = "backup/dialogs/%i.json" % (user['uid'])
+
+    newdata = {
+        'first_name'  : user['first_name'],
+        'last_name'   : user['last_name'],
+        'photo'       : user['photo_max_orig'],
+    }
+
+    if os.path.isfile(filename):
+        data = json.load(open(filename))
+        # Check that latest user data is correct
+        tmp = data['user'].keys()
+        tmp.sort()
+        if len(set(data['user'][tmp[-1]].items()) & set(newdata.items())) != len(newdata):
+            print '    found user data changes - adding new data'
+            data['user'][now] = newdata
+    else:
+        data = {
+            'id'      : user['uid'],
+            'user'    : { now : newdata },
+            'dialogs' : [], # Dialogs massive
+        }
+
+    counter = len(data['dialogs'])
+    while True:
+        msgs = get_messages(user['uid'], counter, token)
+        overall = msgs.pop(0)
+        counter += len(msgs)
+        print '    loaded %i from %i' % (counter, overall)
+
+        # Removing no needed keys from data
+        for msg in msgs:
+            msg.pop('uid', None)
+            msg.pop('read_state', None)
+            msg.pop('from_id', None)
+
+        data['dialogs'].extend(msgs)
+
+        if counter >= overall:
+            break
+
+    with codecs.open(filename, "w", "utf-8") as outfile:
+        json.dump(data, outfile, indent=1, ensure_ascii=False)
 
 print "DONE"
